21129338b6ddbf51ff58b29dd3973ec0704331d7
Devang Chavda <your-email@example.com>
2025-12-27 09:17:45 +0530

    project setup

---
+++ src/Guards/DefaultRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+
+const DefaultRoute = () =>{
+    const authData = JSON.parse(localStorage.getItem("authData"));
+    const role = authData?.role
+    if(!authData){
+        return <Navigate to='/login' replace/>
+    }
+    if(role === "admin"){
+        return <Navigate to='admin/dashboard' replace />
+    }
+    if(role === "user"){
+        return <Navigate to='user/dashboard' replace />
+    }
+}
+
+export default DefaultRoute;
